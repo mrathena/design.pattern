bc4dd5829a317367d8bbfb44a2edc22d18c85a3c
hushirui <hushirui-hb@bestpay.com.cn>
2020-01-09 12:37:51 +0800

    .

--- src/main/java/com/mrathena/design/pattern/creational/patterns/singleton/pattern/implement/枚举/Client.java
+++ src/main/java/com/mrathena/design/pattern/creational/patterns/singleton/pattern/implement/枚举/Client.java
@@ -9,12 +9,11 @@ import com.mrathena.toolkit.ReflectionKit;
 public class Client {
 
 	public static void main(String[] args) {
-		Singleton.INSTANCE.method();
+		System.out.println(Singleton.INSTANCE.hashCode());
 		// 尝试反序列化创建(需实现Serializable)
-		CloneKit.clone(Singleton.INSTANCE).method();
+		System.out.println(CloneKit.clone(Singleton.INSTANCE).hashCode());
+		System.out.println(CloneKit.clone(CloneKit.clone(Singleton.INSTANCE)).hashCode());
 		// 尝试反射创建
-		System.out.println(ReflectionKit.newInstance(Singleton.INSTANCE));
-		System.out.println(ReflectionKit.newInstance(Singleton.INSTANCE));
 		System.out.println(ReflectionKit.newInstance(Singleton.INSTANCE));
 	}
 
